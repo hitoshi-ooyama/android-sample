8db7790ef39dc3a70ca1c2cc48d57f471bf3556e
Hitoshi Ooyama <hitoshi.ooyama@gmo-gc.com>
2016-06-28 11:22:46 +0900

    test

--- app/src/main/java/com/ggee/myapplication/MainActivity.java
+++ app/src/main/java/com/ggee/myapplication/MainActivity.java
@@ -5,11 +5,12 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "TEST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("TEST", "onCreate");
+        Log.d(TAG, "onCreate");
     }
 }
