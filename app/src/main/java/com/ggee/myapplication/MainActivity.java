12334a850289735f74075b2d289b9dbac7a29abc
Hitoshi Ooyama <hitoshi.ooyama@gmo-gc.com>
2016-06-28 13:53:27 +0900

    remove tag

--- app/src/main/java/com/ggee/myapplication/MainActivity.java
+++ app/src/main/java/com/ggee/myapplication/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "TEST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
